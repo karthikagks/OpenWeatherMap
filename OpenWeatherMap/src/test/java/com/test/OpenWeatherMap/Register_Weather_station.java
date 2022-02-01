bf0d4ab66cbbeeb2fbeae1c643d75146961ce20a
karthikagks <34087300+karthikagks@users.noreply.github.com>
2022-02-01 17:18:44 +0530

    Maven included for better dependency management

---
+++ OpenWeatherMap/src/test/java/com/test/OpenWeatherMap/Register_Weather_station.java
@@ -0,0 +1,19 @@
+package com.test.OpenWeatherMap;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Feature"
+		,glue={"teststepdefinition"}
+		,monochrome = true
+		,plugin = { "pretty", "junit:target/cucumber-reports/TestResults.xml" }
+		)
+
+public class Register_Weather_station {
+	
+	
+}
